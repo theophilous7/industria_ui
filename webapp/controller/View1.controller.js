bacd3bc1f62a3d4bca8e195a1644626c3e934422
unknown <theophilous789@gmail.com>
2026-08-27 15:45:24 +0100

    created dashboard to show Manufacturing Lines and Shift status

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,7 +4,27 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("industriaui.controller.View1", {
+
         onInit() {
+        },
+
+        getStatusState(sStatus) {
+
+            switch (sStatus) {
+
+                case "Running":
+                    return "Success";
+
+                case "Delayed":
+                    return "Warning";
+
+                case "Down":
+                    return "Error";
+
+                default:
+                    return "None";
+            }
         }
+
     });
 });
